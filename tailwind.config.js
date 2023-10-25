3f89ed0ae06e03bc5e7e49f31b738a35257e82f4
Mariannedg <mariannedegooijer@gmail.com>
2023-10-25 15:40:25 +0200

    font toegevoegd en kleuren

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+      colors: {
+        // name: "hash code",
+        light: '#f5f5f5',
+      }
+    },
   },
   plugins: [],
-}
-
+};
