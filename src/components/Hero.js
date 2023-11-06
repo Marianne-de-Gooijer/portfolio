44c5d0995fc43bbe4121323f575605dbf03b3c40
Mariannedg <mariannedegooijer@gmail.com>
2023-11-06 18:43:11 +0100

    begin aan de hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,25 @@
 import Image from "next/image";
-import heroImage from "../../public/images/hero-programmer.jpg";
+
+import heroImage from "../../public/images/hero-programmer.webp";
 
 function Hero() {
-    return (
-        <div className="w-screen overflow-hidden h-screen">
-            <Image src={heroImage} alt="/" className="w-full h-full object-cover" />
+  return (
+    <>
+      <section id="home" className="max-w-screen-2xl m-auto">
+        <div className="relative h-screen">
+          <Image
+            src={heroImage}
+            alt="Hero portfolio Marianne de Gooijer"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute top-1/2 right-0 px-10 text-right">
+            <h1 className="text-3xl mb-4">Hey, ik ben Marianne de Gooijer</h1>
+            <p className="text-xl">- Front-end web developer</p>
+          </div>
         </div>
-    )
+      </section>
+    </>
+  );
 }
 
 export default Hero;
