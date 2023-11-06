e4616ab91cba00601bdd48fc9ed721af46507b78
Mariannedg <mariannedegooijer@gmail.com>
2023-11-06 18:45:12 +0100

    wijzigingen hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,14 +5,14 @@ import heroImage from "../../public/images/hero-programmer.webp";
 function Hero() {
   return (
     <>
-      <section id="home" className="max-w-screen-2xl m-auto">
+      <section id="home" className="">
         <div className="relative h-screen">
           <Image
             src={heroImage}
             alt="Hero portfolio Marianne de Gooijer"
             className="w-full h-full object-cover"
           />
-          <div className="absolute top-1/2 right-0 px-10 text-right">
+          <div className="absolute top-1/2 right-0 px-10 text-right max-w-screen-2xl m-auto">
             <h1 className="text-3xl mb-4">Hey, ik ben Marianne de Gooijer</h1>
             <p className="text-xl">- Front-end web developer</p>
           </div>
