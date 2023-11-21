c777d108012a90bf080012a08f7f38b6ae3600b5
Mariannedg <mariannedegooijer@gmail.com>
2023-11-21 15:18:43 +0100

    Hero afgemaakt en begin aan about

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,28 @@
 import Image from "next/image";
 
+import styles from "@/styles/Hero.module.css";
 import heroImage from "../../public/images/hero-programmer.webp";
 
 function Hero() {
   return (
     <>
       <section id="home" className="">
-        <div className="relative h-screen">
+        <div className="relative h-screen flex items-center justify-center text-center">
           <Image
             src={heroImage}
-            alt="Hero portfolio Marianne de Gooijer"
-            className="w-full h-full object-cover"
+            fill
+            className="object-cover"
+            alt="Portfolio Marianne de Gooijer"
           />
-          <div className="absolute top-1/2 right-0 px-10 text-right max-w-screen-2xl m-auto">
-            <h1 className="text-3xl mb-4">Hey, ik ben Marianne de Gooijer</h1>
-            <p className="text-xl">- Front-end web developer</p>
+          <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
+          {/* Optional overlay */}
+          <div className="z-10">
+            <div className="text-white text-4xl sm:text-5xl md:text-6xl text-shadow">
+              <p className={styles.textShadow}>Marianne de Gooijer</p>
+            </div>
+            <p className="text-white text-xl md:text-3xl mt-4">
+              - Front-end developer
+            </p>
           </div>
         </div>
       </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      textShadow: {
+        default: '2px 2px 2px rgba(0, 0, 0, 0.25)',
+      },
       fontFamily: {
         mont: ["var(--font-mont)", ...fontFamily.sans],
       },
