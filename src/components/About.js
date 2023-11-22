0a3a73ace03bae0379330cb1d7c80ef986a1eec9
Mariannedg <mariannedegooijer@gmail.com>
2023-11-22 10:48:14 +0100

    wijzigingen about

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,9 @@ function About() {
       <p className="text-2xl md:text-4xl font-medium text-center mb-12 md:mb-16">
         Over mij
       </p>
-      <div className="flex flex-wrap px-5 md:mb-52 mb-28">
-        <div className="md:w-1/2 w-full md:pr-10 mb-5 md:mb-0">
+      <div className="px-5">
+        <div className="border-b border-black md:border-0 flex flex-wrap md:pb-52 pb-14">
+        <div className="md:w-1/2 w-full md:pr-10 mb-5 md:mb-0 ">
           <div className="md:rounded-full rounded-xl overflow-hidden shadow-xl lg:w-[450px] lg:h-[450px] md:w-[320px] md:h-[320px] sm:max-h-[500px] max-h-[350px] w-auto md:ml-auto">
             <Image
               src={aboutImg1}
@@ -39,8 +40,9 @@ function About() {
             aliquip ex ea commodo consequat.
           </p>
         </div>
+        </div>
       </div>
-      <div className="flex flex-wrap px-5">
+      <div className="flex flex-wrap px-5 pt-14 md:pt-0">
         <div className="md:w-1/2 w-full md:pl-10">
           <p className="leading-relaxed text-md md:text-lg">
             Hier komt meer informatie over mijn hobby&apos;s en over wat ik in mijn vrije tijd vooral doe.
